de5e2333a649900fa422d16b9f8c74a9f32f1fcc
Adam Portilla <adam.portilla@gmail.com>
2012-07-17 22:47:30 -0700

    adding planet class

--- trade-sim.js
+++ trade-sim.js
@@ -1,3 +1,30 @@
+var planet = function(p,my){
+
+	var that = {};
+	my = my || {};
+	p = p || {};
+
+	my.habitability = 50;
+	my.mineralWealth = 1000;
+	my.size = 75;
+
+	that.population = 0;
+	that.commerce = 0;
+	that.agriculture = 0;
+	that.mining = 0;
+	that.manufacturing = 0;
+
+	that.directive = 'balance';
+
+	that.turn = function(){
+		// calculate deltas in planet properties
+		// 
+	};
+
+	return that;
+};
+
+
 var tradesim = function(){
 
   
