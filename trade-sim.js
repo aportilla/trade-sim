43d9a5ef6858dec504c6d987ae6132fba3679297
Adam <contact.adam.portilla@gmail.com>
2012-07-19 22:00:12 -0700

    adding start of graph object

--- trade-sim.js
+++ trade-sim.js
@@ -1,31 +1,90 @@
-var planet = function(p,my){
-
-	var that = {};
-	my = my || {};
-	p = p || {};
-
-	my.habitability = 50;
-	my.mineralWealth = 1000;
-	my.size = 75;
-
-	that.population = 0;
-	that.commerce = 0;
-	that.agriculture = 0;
-	that.mining = 0;
-	that.manufacturing = 0;
-
-	that.directive = 'balance';
-
-	that.turn = function(){
-		// calculate deltas in planet properties
-		// 
-	};
-
-	return that;
+var makeId = function() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for( var i=0; i < 5; i++ )
+  text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
 };
 
+var Model = function(defaults,my){
+	
+	defaults = defaults || {};
+
+	return function(p,my){
+		var that = that || {};
+		// set obj props based on defaults...
+		return that;
+	};
+
+};
+
+var Planet = new Model({
+	foodSupply : 0
+});
+
+var Ship = new Model({
+	cargo : '',
+	units : 0
+});
+
+
+var Graph = function(){
+
+	var nodes = {};
+	var edges = [];
+
+	var Edge = function(start,end,that){
+		that = that || {};
+		var start = [];
+		return that;
+	};
+
+	var Node = function(that){
+		that = that || {};
+		that.edges = [];
+		return that;
+	};
+
+	return {
+		addEdge : function(start,end){
+			// make nodes if not yet in graph
+			if (!nodes[start]){ nodes[start] = new Node(); }
+			if (!nodes[end]){ nodes[end] = new Node(); }
+
+			var newEdge = new Edge(start,end);			
+
+			edges.push(newEdge);
+			nodes[start].edges.push(newEdge);
+			nodes[end].edges.push(newEdge);
+
+		}
+	};
+};
 
 var tradesim = function(){
 
+	var planetA = new Planet({ 
+		foodSupply : 60,
+	});
+	
+	var planetB = new Planet({ 
+		foodSupply : -20
+	});
+	
+	var planetC = new Planet({ 
+		foodSupply : -30
+	}));
+
+	var starChart = new Graph();
+
+	starChart.addEdge(planetA.id,planetB.id);
+	starChart.addEdge(planetB.id,planetC.id);
+	starChart.addEdge(planetC.id,planetA.id);
+
+	return {
+		turn : function(){
+
+		};
+	};
   
 }();
